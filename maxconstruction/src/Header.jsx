6850e0b2a6e4d9bcfec8ba55546363a8112c628f
Tshepiso Twala <tshepisotwalaj@gmail.com>
2026-06-01 16:26:03 +0200

    Update Header.jsx, Class naming convention

--- maxconstruction/src/Header.jsx
+++ maxconstruction/src/Header.jsx
@@ -1,13 +1,13 @@
 function Header() {
     return ( //pure html
         <header className="header">
-            <h1 className="header__title">Max's Construction</h1>
-            <nav className="header__nav">
-                <ul className="header__nav-list">
-                    <li className="header__nav-item"><a href="#home" className="header__nav-link">Home</a></li>
-                    <li className="header__nav-item"><a href="#services" className="header__nav-link">Services</a></li>
-                    <li className="header__nav-item"><a href="#projects" className="header__nav-link">Projects</a></li>
-                    <li className="header__nav-item"><a href="#contact" className="header__nav-link">Contact</a></li>
+            <h1 className="header_title">Max's Construction</h1>
+            <nav className="header_nav">
+                <ul className="header_nav-list">
+                    <li className="header_nav-item"><a href="#home" className="header_nav-link">Home</a></li>
+                    <li className="header_nav-item"><a href="#services" className="header_nav-link">Services</a></li>
+                    <li className="header_nav-item"><a href="#projects" className="header_nav-link">Projects</a></li>
+                    <li className="header_nav-item"><a href="#contact" className="header_nav-link">Contact</a></li>
                 </ul>
             </nav>
         </header>
