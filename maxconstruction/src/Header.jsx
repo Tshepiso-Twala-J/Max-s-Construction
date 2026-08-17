fbd816de1dae39e76b61e5000360d69aa7f76d50
Tshepiso Twala <tshepisotwalaj@gmail.com>
2026-08-17 08:55:42 +0200

    Update Header.jsx
    Added a Nav Link

--- maxconstruction/src/Header.jsx
+++ maxconstruction/src/Header.jsx
@@ -5,7 +5,8 @@ function Header() {
             <nav className="header_nav">
                 <ul className="header_nav-list">
                     <li className="header_nav-item"><a href="#home" className="header_nav-link">Home</a></li>
-                    <li className="header_nav-item"><a href="#services" className="header_nav-link">Services</a></li>
+<li className = "header_nav-item"><a href="#portfolio" className="header_nav-link>Portfolio</a><
+                    </li> className="header_nav-item"><a href="#services" className="header_nav-link">Services</a></li>
                     <li className="header_nav-item"><a href="#projects" className="header_nav-link">Projects</a></li>
                     <li className="header_nav-item"><a href="#contact" className="header_nav-link">Contact</a></li>
                 </ul>
